4a79a2441e40cac2a69e70c4e813bfd032a0eb36
Francois Visser <francois.haf@gmail.com>
2020-04-07 20:10:45 +0000

    before refactoring

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 var express       = require("express"),
     app           = express(),
 	bodyParser    = require("body-parser"),
-	mongoose      = require("mongoose")
+	mongoose      = require("mongoose"),
+	Picture       = require("./models/picture"),
+	passport      = require("passport"),
+	LocalStrategy = require("passport-local"),
+	User          = require("./models/user")
+
+
     
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/pinterest_vibes", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-var pictureSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
+app.use(require("express-session")({
+	secret: "This is my secret!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-var Picture = mongoose.model("picture", pictureSchema); 
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	next();
+})
  
 
 app.get("/", function(req, res){
@@ -27,12 +42,12 @@ app.get("/home", function(req, res){
 		if(err){
 			console.log(err);
 		} else{
-			res.render("index", {pictures: pictures});
+			res.render("pictures/index", {pictures: pictures, currentUser: req.user});
 		}
 	});
 });
 
-app.post("/home", function(req, res){
+app.post("/home", isLoggedIn, function(req, res){
 	var name  = req.body.name;
 	var image = req.body.image;
 	var desc  = req.body.description;
@@ -46,8 +61,8 @@ app.post("/home", function(req, res){
 	});
 });
 
-app.get("/home/new", function(req, res){
-	 res.render("new");
+app.get("/home/new", isLoggedIn, function(req, res){
+	 res.render("pictures/new");
 });
 
 app.get("/home/:id", function(req, res){
@@ -55,13 +70,80 @@ app.get("/home/:id", function(req, res){
 		if(err){
 			console.log(err);
 		} else{
-			res.render("show", {picture: foundPicture});
+			res.render("pictures/show", {picture: foundPicture});
 		}
 	});
 });
 
+// app.get("/home/:id", function(req, res){
+// 	Picture.findById(req.params.id, function(err, picture){
+// 		if(err){
+// 			console.log(err);
+// 		} else{
+// 			res.render("pictures/show", {picture: picture});
+// 		}
+// 	});
+// });
+
+// app.post("home/:id", function(req, res){
+// 	Picture.findById(req.params.id, function(err, picture){
+// 		if(err){
+// 			console.log(err);
+// 		} else{
+// 			Comment.create(req.body.comment, function(err, comment){
+// 				if(err){
+// 					console.log(err);
+// 				} else{
+// 					picture.comments.push(comment);
+// 					picture.save();
+// 					res.redirect("/show/" + picture._id)
+// 				}
+// 			});
+// 		}
+// 	});
+// });
 
 
+app.get("/register", function(req, res){
+	res.render("register")
+});
+
+app.post("/register", function(req, res){
+	var newUser = new User({username:req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		} 
+			passport.authenticate("local",)(req, res, function(){
+				res.redirect("/home")
+		});
+	});
+});
+
+app.get("/login", function(req, res){
+	res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", 
+		{
+	     successRedirect: "/home",
+	     failureRedirect: "/login"
+	}),	function(req, res){
+	
+});
+ 
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/home");
+});
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 app.listen("3000", function(){
 	console.log("Pinterest is running");
